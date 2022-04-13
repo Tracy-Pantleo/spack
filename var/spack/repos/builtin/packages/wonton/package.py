fd72716b6e7577529ac6905ecc922e2e9e34c30f
Tracy-Pantleo <91685320+Tracy-Pantleo@users.noreply.github.com>
2022-04-13 11:48:26 -0600

    Update package.py
    test

--- var/spack/repos/builtin/packages/wonton/package.py
+++ var/spack/repos/builtin/packages/wonton/package.py
@@ -4,7 +4,7 @@
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
 from spack import *
-from spack.pkg.builtin.boost import Boost
+#from spack.pkg.builtin.boost import Boost
 
 
 class Wonton(CMakePackage):
@@ -61,7 +61,7 @@ class Wonton(CMakePackage):
     # TODO: replace this with an explicit list of components of Boost,
     # for instance depends_on('boost +filesystem')
     # See https://github.com/spack/spack/pull/22303 for reference
-    depends_on(Boost.with_default_variants, when='@:1.2.10 ~thrust')
+    #depends_on(Boost.with_default_variants, when='@:1.2.10 ~thrust')
 
     # CUDA library
     depends_on('cuda', when='+cuda')
